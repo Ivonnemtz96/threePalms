d846d59060b550cff42b63cab167baf3eb4f39b2
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-11-09 16:15:55 -0700

    header y title ok

--- includes/header.php
+++ includes/header.php
@@ -18,91 +18,13 @@
                         </div>
                         <div class="menu-area">
                             <ul class="menu">
-                                <li>
-                                    <a href="javascript:void(0);">Homes</a>
-                                    <ul class="submenu">
-                                        <li><a href="index.html">Home 1</a></li>
-                                        <li><a href="index-2.html">Home 2</a></li>
-                                        <li><a href="index-3.html">Home 3</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="about.html">About</a></li>
-                                <li>
-                                    <a href="javascript:void(0);">Rooms</a>
-                                    <ul class="submenu">
-                                        <li><a href="room-listing1.html">Room Listing 1</a></li>
-                                        <li><a href="room-listing2.html">Room Listing 2</a></li>
-                                        <li><a href="room-listing3.html">Room Listing 3</a></li>
-                                        <li><a href="room-details.html">Room Details</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="javascript:void(0);">Pages</a>
-                                    <ul class="submenu">
-                                        <li><a href="about.html">About</a></li>
-                                        <li><a href="booking.html">Booking</a></li>
-                                        <li><a href="pricing.html">Pricing</a></li>
-                                        <li><a href="faq.html">FAQ</a></li>
-                                        <li>
-                                            <a href="javascript:void(0);">Rooms</a>
-                                            <ul class="submenu">
-                                                <li><a href="room-listing1.html">Room Listing 1</a></li>
-                                                <li><a href="room-listing2.html">Room Listing 2</a></li>
-                                                <li><a href="room-listing3.html">Room Listing 3</a></li>
-                                                <li><a href="room-details.html">Room Details</a></li>
-                                            </ul>
-                                        </li>
-                                        <li>
-                                            <a href="javascript:void(0);">Services</a>
-                                            <ul class="submenu">
-                                                <li><a href="services.html">All Services</a></li>
-                                                <li><a href="service-details.html">Service Details</a></li>
-                                            </ul>
-                                        </li>
-                                        <li>
-                                            <a href="javascript:void(0);">Staffs</a>
-                                            <ul class="submenu">
-                                                <li><a href="team.html">All Staffs</a></li>
-                                                <li><a href="team-single.html">Staff Details</a></li>
-                                            </ul>
-                                        </li>
-                                        <li>
-                                            <a href="javascript:void(0);">Galleries</a>
-                                            <ul class="submenu">
-                                                <li><a href="gallery-1.html">Gallery 1</a></li>
-                                                <li><a href="gallery-2.html">Gallery 2</a></li>
-                                                <li><a href="gallery-3.html">Gallery 3</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="account.html">Account</a></li>
-                                        <li><a href="404.html">404</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="javascript:void(0);">Blogs</a>
-                                    <ul class="submenu">
-                                        <li><a href="blog.html">Blog</a></li>
-                                        <li><a href="blog-single.html">Blog Single</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="contact.html">Contact</a></li>
+                                <li><a href="contact.html">Inicio</a></li>
+                                <li><a href="contact.html">Nosotros</a></li>
+                                <li><a href="contact.html">Rentas</a></li>
+                                <li><a href="contact.html">Galería</a></li>
+                                <li><a href="contact.html">Contacto</a></li>
                             </ul>
-                            <div class="search-container">
-                                <a href="#" class="search-btn"><i class="fa-solid fa-magnifying-glass"></i></a>
-
-                                <div class="search-bar">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control" placeholder="Search Here"
-                                            aria-label="Search Input" aria-describedby="basic-addon2">
-                                        <button class="input-group-text" id="basic-addon2">Search</button>
-                                    </div>
-
-                                </div>
-                            </div>
-
-
-                            <a href="booking.html" class="booking-btn"><span>Book
-                                    Now</span></a>
+                            <a href="booking.html" class="booking-btn"><span>Ir a reservar</span></a>
 
                             <!-- toggle icons -->
                             <div class="header-bar d-lg-none">
