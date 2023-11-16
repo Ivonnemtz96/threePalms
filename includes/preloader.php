01d733963ddbfab23cfbde450205100b427dce8c
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-11-15 17:06:23 -0700

    Se suben archivos?

--- includes/preloader.php
+++
@@ -1,5 +0,0 @@
- <!-- ===============>> Preloader start here <<================= -->
- <div class="preloader">
-     <div class="loader"></div>
- </div>
- <!-- ===============>> Preloader end here <<================= -->
