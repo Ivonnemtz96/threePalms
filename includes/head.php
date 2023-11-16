c26a95cb5860fa8ce18d2c94c53996eec9806768
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-11-16 16:59:37 -0700

    se agrega desc

--- includes/head.php
+++ includes/head.php
@@ -3,6 +3,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Three Palms - <? echo $title; ?></title>
+    <meta name="description" content="Three palms is a Property management & Vacation planning agency that is here to change the game.">
 
     <link rel="shortcut icon" href="assets/images/favicon2.png?=1" type="image/x-icon">
 
